05afe21ae81e7327c8da4a00bc315baa2da8aa91
yousa <yousa@penguin>
2015-07-13 18:13:23 +0900

    Starting project.

---
+++ fload.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+""" Load data from File. """
+
+import sys
+import numpy as np
+
+import util
+
+
+def __parse_flags__(flags):
+    """ parse file format flags """
+    r = ""
+    e = fmt[2:]
+    if   e == "le": r = "<"
+    elif e == "be": r = ">"
+    elif e == "ne": r = "="
+    else:
+        raise NameError("The encoding %s is unknown." % fmt)
+    
+    return r + fmt[0:2]
+
+
+def fload(f, fmt, ndim=None):
+    """ Load data from file.
+    parameter f:    File like object or filename.
+    parameter fmt:  File format flags. The follwing are the details.
+    parameter ncol: The number of column to reshape. If False or None, Nothing to do.
+
+    fmt
+    ---
+    The format flags is three type. If the file is ascii text format, the flag is 'text'. If the file is numpy binary format, the flag is 'npy'.
+    If the file is binary format, the flag is combination of encoding and endian(e.g. f4le means little nedian 4byte float).
+
+    The encoding is concatecated 2 characters. The first one is the kind of data. The second one is the number of byte per one sample. 
+    Available kind is follow:
+        i: int
+        u: unsigned int
+        f: float
+   The endian is follow:
+        le: little endian
+        be: big endian
+        ne: native endian
+    """
+
+    if fmt == "npy":
+        return np.load(f)
+    elif fmt == "text":
+        return np.loadtxt(f)
+    else:
+        dtype = __parse_flags__(fmt)
+        m = np.fromfile(f, dtype)
+        if ndim:
+            np.reshape(m, (-1, ndim))
+        return m
+
+
+if __name__=='__main__':
+    if len(sys.argv) < 2:
+        sys.stdout.write(fload.__doc__+"\n")
+
+    fmt_flag = sys.argv[1]
+    for fname in sys.argv[2:]:
+        d = fload(fname, fmt_flag)
+        np.savetxt(sys.stdout, d, fmt='%+.6e')
+
---
+++ util.py
@@ -0,0 +1,8 @@
+#!/usr/bin python
+
+import sys
+
+def stdout(s):
+    sys.stdout.write(s)
+def stderr(s):
+    sys.stderr.write(s)
